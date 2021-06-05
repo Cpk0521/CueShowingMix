40eba4a17c87932095f86687c953f2e7ac332a7d
CPK <samkit157@gmail.com>
2021-06-05 16:50:37 +0800

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const fs = require('fs');
 const { endianness } = require('os');
 const FF = require('./ffmpeg');
-// const { exec } = require('child_process');
+const { exec } = require('child_process');
 
 var app = express();
 const PORT = process.env.PORT || 3000;
@@ -22,34 +22,34 @@ app.get('/', (req, res)=>{
 
 app.get('/mixer', (req, res, next)=>{
 
-    req.setTimeout(500000, ()=>{
-
-        let output = Date.now() + ".mp4"
     
-        let sid = req.query.id;
-        let char1 = req.query.char1;
-        let char2 = req.query.char2;
-        let char3 = req.query.char3;
-        let char4 = req.query.char4;
+    req.setTimeout(500000);
+    
+    let output = Date.now() + ".mp4";
 
-        // res.send(`Composing the Video now, please waiting 1-2 mins`)
+    let sid = req.query.id;
+    let char1 = req.query.char1;
+    let char2 = req.query.char2;
+    let char3 = req.query.char3;
+    let char4 = req.query.char4;
 
-        FF.FFMixer(sid, jsons, char1, char2, char3, char4, output).then(()=>{
-            console.log("file is converted");
+    // res.send(`Composing the Video now, please waiting 1-2 mins`)
 
-            res.download(output, (err) => {
-                if(err) console.log(err)
+    FF.FFMixer(sid, jsons, char1, char2, char3, char4, output).then(()=>{
+        console.log("file is converted");
 
-                fs.unlinkSync(output);
-                next();
-            });
+        res.download(output, (err) => {
+            if(err) console.log(err)
 
-        }).catch(()=>{
-            console.log('ff error');
+            fs.unlinkSync(output);
+            next();
         });
 
+    }).catch(()=>{
+        console.log('ff error');
     });
-    next();
+    
+
 })
 
 
